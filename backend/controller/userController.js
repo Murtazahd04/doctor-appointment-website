3fa0742b4844fa939e332be2b8d5bc0b7fad6e02
Murtaza Dhanerawala <murtazahd04@gmail.com>
2025-10-22 22:30:20 +0530

    Display and update patient profile data on website

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -2,6 +2,8 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
+import {v2 as cloudinary}from 'cloudinary'
+
 //API to register user
 const registerUser = async(req,res)=>{
     try {
@@ -78,4 +80,45 @@ const loginUser = async(req, res) => {
 
     }    
 }
-export {registerUser,loginUser}
+
+// API TO get user profile data 
+const getProfile = async(req,res)=>{
+    try {
+        const {userId} = req.userId
+        const userData = await userModel.findById(req.userId).select('-password')
+
+        res.json({success:true,userData}
+        )
+
+    } catch (error) {
+          console.log(error);
+        res.json({success:false ,message:error.message})
+
+    }
+}
+
+// API TO UPDATE USER PROFILE
+const updateProfile = async(req,res)=>{
+    try {
+        const {userId, name , phone ,address,dob,gender } = req.body
+        const imageFile = req.file
+
+        if (!name || !phone ||!dob || !!gender) {
+            return res.json({success:false,message:"Data Missing"})
+        }
+        
+        await userModel.findByIdAndUpdate(userId,{name,phone,address:JSON.parse(address),dob,gender})
+        if (imageFile) {
+            
+            //uploud image to cloudinary
+            const imageUploud = await cloudinary.uplouder.uploud(imageFile.path,{resource_type:'image'})
+            
+
+        }
+    } catch (error) {
+         console.log(error);
+        res.json({success:false ,message:error.message})
+
+    }
+}
+export {registerUser,loginUser,getProfile}
