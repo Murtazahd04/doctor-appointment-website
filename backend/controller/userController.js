cbfedbe6c2f6560593e7267c39fc4b6e79e3b7df
Murtaza Dhanerawala <murtazahd04@gmail.com>
2025-10-22 21:25:40 +0530

    Created Patient Login and Registration system

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,3 @@
-
 import validator from 'validator'
 import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
@@ -48,7 +47,35 @@ const registerUser = async(req,res)=>{
          console.log(error);
         res.json({success:false ,message:error.message})
 
-    }
+    } 
 }
 
-export {registerUser}
+
+// API FOR USER LOGIN 
+const loginUser = async(req, res) => {
+    try {
+        
+        const {email,password} = req.body
+        const user = await userModel.findOne({email})
+
+        if (!user) {
+            return res.json({success:false ,message:'User does not exist'})
+        } else {
+            
+        }
+        const isMatch = await bcrypt.compare(password,user.password)
+
+        if (isMatch) {
+            const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+            res.json({success:true,token})
+        } else {
+            res.json({success:false,message:"Invalid Credentials"})
+        }
+
+    } catch (error) {
+         console.log(error);
+        res.json({success:false ,message:error.message})
+
+    }    
+}
+export {registerUser,loginUser}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
 
-import { registerUser } from '../controller/userController.js' 
+import { registerUser,loginUser } from '../controller/userController.js' 
 
 const userRouter = express.Router()
 
 userRouter.post('/register',registerUser)
+userRouter.post('/login',loginUser)
+
 
 
 export default userRouter
