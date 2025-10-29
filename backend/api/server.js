ec44f3e03b176b9fe731af5468dc4bf4be749ed4
Huzefa Ismail <huzefa@rajinfosys.net>
2025-10-29 16:15:06 +0530

    editing done

--- backend/api/server.js
+++ backend/api/server.js
@@ -17,7 +17,7 @@ connectCloudinary()
 // middlewares
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://doctorappointmentwebsitefrontend.vercel.app',
 }))
 
 
